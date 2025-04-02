cd87fb919dbfe6fc36051fa87d7d7bea5a9bd9a3
jaiswalaman2020 <jaiswalaman2020@gmail.com>
2025-04-02 13:43:25 +0530

    updated settings page

--- frontend/src/pages/SettingsPage.jsx
+++ frontend/src/pages/SettingsPage.jsx
@@ -66,7 +66,7 @@ const SettingsPage = () => {
                       J
                     </div>
                     <div>
-                      <h3 className="font-medium text-sm">John Doe</h3>
+                      <h3 className="font-medium text-sm">Aman Jaiswal</h3>
                       <p className="text-xs text-base-content/70">Online</p>
                     </div>
                   </div>
